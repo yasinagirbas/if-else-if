3fc3ff05f51ea80631c29e4cb12a2ae493442baf
yasinagirbas <yasin.agirbas@hotmail.com>
2022-08-18 14:12:20 +0300

    first commit

---
+++ if-else-if/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace if_else_if
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if (time>=6 && time<11)
+            {
+                Console.WriteLine("günaydın");
+            }
+            else if (time<=18)
+            {
+               Console.WriteLine("iyi günler"); 
+            }
+            else 
+            {
+                Console.WriteLine("iyi geceler");
+            }
+
+            string sonuc = time <= 18 ? "iyi günler" : "iyi geceler";
+            Console.WriteLine(sonuc);
+            sonuc = time > 06 && time < 11 ? "gunyadin" :
+                time <= 18 ? "iyi günler" : "iyi geceler"; //Burada soru işareti if demektik. : de else demek  
+Console.WriteLine(sonuc);
+        }
+    }
+}
